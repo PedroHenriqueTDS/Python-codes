9b2c8e89f78f3d36a53bbd57f00920e8baa838ed
Pedro Henrique Trajano da Silva <134293745+PedroHenriqueTDS@users.noreply.github.com>
2024-04-06 23:22:56 -0300

    Update terreno.py

--- terreno.py
+++ terreno.py
@@ -1,1 +1,9 @@
+def calcularArea(comprimento, largura):
+    area = comprimento * largura
+    return area
 
+comprimento = float(input("Digite o comprimento do terreno em metros: "))
+largura = float(input("Digite a largura do terreno em metros: "))
+
+area_terreno = calcularArea(comprimento, largura)
+print("A área do terreno é: {:.2f} metros quadrados".format(area_terreno))
